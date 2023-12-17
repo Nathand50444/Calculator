c1459c3e4298fc5339a6a7aec4b57c34bbb0d677
NathanD <nathand50444@gmail.com>
2023-12-17 19:11:46 +0000

    Calculator javascript complete
    Allows for +,-,* and / operators. Error for division by zero. Color change on click of buttons, and footer added.

--- script.js
+++ script.js
@@ -1,5 +1,87 @@
-function appendToOutput(value) {
-    document.getElementById("output").value += value;
-}
+let displayValue = '0';
+let operator = '';
+let operand = '';
 
-// See https://www.youtube.com/watch?v=cGgLHJGyS34
+    function getNumber(number) {
+        if (displayValue === '0') {
+            displayValue = number;
+        } else {
+            displayValue += number;
+        }
+        updateDisplay();
+        const clickedButton = event.target;
+
+    clickedButton.style.backgroundColor = 'grey';
+
+    setTimeout(() => {
+        clickedButton.style.backgroundColor = '';
+    }, 100);
+    }
+
+    function getOperator(op) {
+        operator = op;
+        operand = displayValue;
+        displayValue = '0';
+        updateDisplay();
+        const clickedButton = event.target;
+
+        clickedButton.style.backgroundColor = 'grey';
+    
+        setTimeout(() => {
+            clickedButton.style.backgroundColor = '';
+        }, 100);
+    }
+
+    function calculateResult() {
+        let result;
+        const clickedButton = event.target;
+
+        clickedButton.style.backgroundColor = 'grey';
+    
+        setTimeout(() => {
+            clickedButton.style.backgroundColor = '';
+        }, 100);
+        switch (operator) {
+            case '+':
+                result = parseFloat(operand) + parseFloat(displayValue);
+                break;
+            case '-':
+                result = parseFloat(operand) - parseFloat(displayValue);
+                break;
+            case '*':
+                result = parseFloat(operand) * parseFloat(displayValue);
+                break;
+            case '÷':
+                if (parseFloat(displayValue) !== 0) {
+                    result = parseFloat(operand) / parseFloat(displayValue);
+                } else {
+                    alert("Cannot divide by zero");
+                    clearDisplay();
+                    return;
+                }
+                break;
+        }
+        displayValue = result.toString();
+        updateDisplay();
+        operator = '';
+        operand = '';
+    }
+
+    function clearDisplay() {
+        displayValue = '0';
+        operator = '';
+        operand = '';
+        updateDisplay();
+        const clickedButton = event.target;
+
+        clickedButton.style.backgroundColor = 'grey';
+    
+        setTimeout(() => {
+            clickedButton.style.backgroundColor = '';
+        }, 100);
+    }
+
+    function updateDisplay() {
+        document.getElementById('output').value = displayValue;
+    }
+    
